f56e65caa1b5682be8f574d091dbd6e718caf7d5
NataliaPavlova87 <42961335+NataliaPavlova87@users.noreply.github.com>
2021-10-02 14:46:00 +0300

    Create UiButton.vue
    add new components

---
+++ src/components/uiComponents/UiButton/UiButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button class="button" title="text">
+    <span class="button__text">
+      {{ text }}
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "UiButton",
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #eeeeee;
+  border-radius: 10px;
+  padding: 10px;
+  border: 1px solid transparent;
+  transition: opacity 0.2s, border-color 0.2s;
+  font-family: "Inter", sans-serif;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  &:focus {
+    border: 1px solid hsl(0, 0%, 71%);
+  }
+
+  &__text {
+    display: block;
+    max-width: 100%;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    font-weight: 600;
+    font-size: 12px;
+    text-align: center;
+    letter-spacing: -0.02em;
+    color: hsl(0, 0%, 71%);
+  }
+}
+</style>
