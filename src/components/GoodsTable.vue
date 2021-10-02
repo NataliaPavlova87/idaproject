ad3e80abafb4f181ff2c639ea4bc3da8ea255cbd
NataliaPavlova87 <42961335+NataliaPavlova87@users.noreply.github.com>
2021-10-02 14:45:43 +0300

    Create GoodsTable.vue
    add new components

---
+++ src/components/GoodsTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="goods-table">
+    <h2 class="goods-table__title">Товары</h2>
+    <Product
+      v-for="product in goods"
+      :key="`goods-table__item--${product.id}`"
+      @removeItem="$emit('removeItem', product)"
+      :product="product"
+    ></Product>
+  </section>
+</template>
+
+<script>
+import Product from "../components/Product.vue";
+
+export default {
+  name: "GoodsTable",
+  props: {
+    goods: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    Product,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.goods-table {
+  display: grid;
+  grid-gap: 16px;
+  grid-template-columns: repeat(auto-fit, minmax(330px, 1fr));
+
+  @media (max-width: 768px) {
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  }
+
+  &__title {
+    display: none;
+  }
+}
+</style>
